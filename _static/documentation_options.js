ca780b8c146d3d9a9cc5bc0c2c0e1c5bde0421e2
Randy LeVeque <rjl@uw.edu>
2019-05-25 21:09:24 -0700

    update dev docs from clawpack/doc@6e427d012 with new adjoint flagging docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.5.0_future',
+    VERSION: '5.5.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
