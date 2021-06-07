0bb3fa44306ee33ea43fdd68ed720efc7d01abdd
Randy LeVeque <rjl@uw.edu>
2021-06-07 14:51:48 -0400

    added IslandBuffering notebook

--- gallery/_static/documentation_options.js
+++ gallery/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.7.x',
+    VERSION: '5.8.x',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
